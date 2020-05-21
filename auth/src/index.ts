5ee051e0720ab7a8edd45ddd708d7f95f765854b
Dan Kreiger <dan@dankreiger.com>
2020-05-21 18:23:10 +0200

    feat(auth): add user model

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -24,6 +26,21 @@ app.all('*', async () => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Auth puppy service listening on port ${PORT}`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('connected to mongodb :)');
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Auth puppy service listening on port ${PORT}`);
+  });
+};
+
+start();
