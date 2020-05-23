9ebf42522feaa3032678aa99e3f2d4643a56cbba
Dan Kreiger <dan@dankreiger.com>
2020-05-23 16:54:01 +0200

    test(auth): add more auth tests

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,40 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+describe('signin', () => {
+  it('fails when an email that does not exist is supplied', async () => {
+    await request(app)
+      .post('/api/users/signin')
+      .send({
+        email: 'test@test.com',
+        password: 'password',
+      })
+      .expect(400);
+  });
+
+  it('fails when an incorrect password is supplied', async () => {
+    await request(app)
+      .post('/api/users/signup')
+      .send({ email: 'test@email.com', password: 'password' })
+      .expect(201);
+
+    await request(app)
+      .post('/api/users/signin')
+      .send({ email: 'test@email.com', password: 'password1' })
+      .expect(400);
+  });
+
+  it('responsed with a cookie when given valid credentials', async () => {
+    await request(app)
+      .post('/api/users/signup')
+      .send({ email: 'test@email.com', password: 'password' })
+      .expect(201);
+
+    const response = await request(app)
+      .post('/api/users/signin')
+      .send({ email: 'test@email.com', password: 'password' })
+      .expect(200);
+
+    expect(response.get('Set-Cookie')).toBeDefined();
+  });
+});
