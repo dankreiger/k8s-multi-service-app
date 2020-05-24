86ed1a722402c2d25c3c48c120cac9cda043704f
Dan Kreiger <dan@dankreiger.com>
2020-05-24 20:27:34 +0200

    chore(app): add header and signin/signout pages

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
+
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signin',
+    method: 'post',
+    body: { email, password },
+    onSuccess: () => Router.push('/'),
+  });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    doRequest();
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Signin</h1>
+      <div className="form-group">
+        <label htmlFor="email">Email Address</label>
+        <input
+          type="text"
+          id="email"
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      {errors}
+      <button className="btn btn-primary">Sign In</button>
+    </form>
+  );
+};
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,20 +1,14 @@
 import Link from 'next/link';
 import buildClient from '../api/build-client';
 
-const LandingPage = ({ currentUser }) => {
-  return (
-    <>
-      <Link href="/auth/signup">signup</Link>
-      {currentUser ? (
-        <h3>
-          You are signed in as: <code>{currentUser?.email}</code>
-        </h3>
-      ) : (
-        <h3>You are not signed in</h3>
-      )}
-    </>
+const LandingPage = ({ currentUser }) =>
+  currentUser ? (
+    <h3>
+      You are signed in as: <code>{currentUser?.email}</code>
+    </h3>
+  ) : (
+    <h3>You are not signed in</h3>
   );
-};
 
 LandingPage.getInitialProps = async (context) => {
   const client = buildClient(context);
