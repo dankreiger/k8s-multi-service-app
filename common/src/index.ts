5dfde869efaa4bba7b7e5a095aefd6cb43ea6a1e
Dan Kreiger <dan@dankreiger.com>
2020-05-24 23:59:39 +0200

    chore(package): update common package

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,13 +1,11 @@
-interface Puppy {
-  tail: string;
-  head: number;
-}
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
 
-const puppy: Puppy = {
-  tail: '4',
-  head: 4,
-};
-
-console.log(puppy);
-
-export default puppy;
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
